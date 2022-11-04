a58ced33c3db0ccf0d93c0a89bb4cada2f51fd8f
Aditya Pathak <adityapathak@Administrators-MacBook-Pro.local>
2022-11-05 04:56:15 +0530

    adding custom routes to pocketbase

--- backend/main.go
+++ backend/main.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	log.Println("Hello World")
 	app := pocketbase.New()
 
+	app.RootCmd.AddCommand(&cobra.Command{
+		Use: "hello",
+		Run: func(command *cobra.Command, args []string) {
+			log.Println("Hello world!")
+		},
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/golang/dojo",
+			Handler: func(c echo.Context) error {
+				return c.String(200, "Hello Gophers!")
+			},
+		})
+
+		return nil
+	})
+
 	if err := app.Start(); err != nil {
-		log.Fatalf(err)
+		log.Fatal(err)
 	}
 }
